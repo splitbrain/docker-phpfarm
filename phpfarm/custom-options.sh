f7c256592c39439d500e4da967b10d7aef34bb23
Andreas Gohr <andi@splitbrain.org>
2016-03-04 15:37:20 +0100

    enabled more common extensions (wheezy)

--- phpfarm/custom-options.sh
+++ phpfarm/custom-options.sh
@@ -3,22 +3,36 @@ if [ -d "/lib/i386-linux-gnu" ]; then
     LIBPATH="/lib/i386-linux-gnu/"
 else
     LIBPATH="/lib/x86_64-linux-gnu/"
+
+    # fix problem with imap http://bit.ly/1WZcTWD
+    ln -s /usr/lib/libc-client.a /usr/lib/x86_64-linux-gnu/libc-client.a
 fi
 
 
 configoptions="$configoptions \
-    --with-pdo-mysql \
     --enable-fastcgi \
+    --enable-gd-native-ttf \
+    --enable-intl \
+    --with-ldap \
+    --with-ldap-sasl \
     --with-bz2 \
     --with-curl \
+    --with-imap \
+    --with-imap-ssl \
     --with-gd \
     --with-jpeg-dir=/usr/lib \
-    --with-png-dir=/usr/lib \
-    --enable-gd-native-ttf \
-    --with-ttf \
-    --with-mhash \
-    --with-mcrypt \
+    --with-kerberos \
     --with-libdir=$LIBPATH \
+    --with-mcrypt \
+    --with-mhash \
+    --with-mysql=/usr \
+    --with-mysqli=/usr/bin/mysql_config \
+    --with-openssl \
+    --with-pdo-mysql \
+    --with-png-dir=/usr/lib \
+    --with-pgsql \
+    --with-ttf \
+    --with-xsl=/usr \
 "
 
 echo $configoptions
