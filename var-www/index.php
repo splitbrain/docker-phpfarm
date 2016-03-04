094a68e590496579e576034e1a9663d2406acc12
Andreas Gohr <andi@splitbrain.org>
2016-03-04 15:37:53 +0100

    easier way to query enabled extensions

--- var-www/index.php
+++ var-www/index.php
@@ -1,1 +1,10 @@
-<?php phpinfo() ?>
+<?php
+if(isset($_GET['modules'])) {
+    header('Content-Type: text/plain');
+    $mods = get_loaded_extensions();
+    $mods = array_map('strtolower', $mods);
+    sort($mods);
+    echo join("\n", $mods);
+} else {
+    phpinfo();
+}
