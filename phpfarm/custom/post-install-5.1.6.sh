e348048bf6fcad6f7a66b933e1cb2f0d14f5a634
Eugene Sia <eugene@eugenesia.co.uk>
2017-03-26 03:48:52 +0100

    Add post-install steps for PHP 5.1

---
+++ phpfarm/custom/post-install-5.1.6.sh
@@ -0,0 +1,6 @@
+
+# The PHP-CLI executable is also the CGI executable.
+ln -fs /phpfarm/inst/php-5.1.6/bin/php /phpfarm/inst/php-5.1.6/bin/php-cgi
+
+ln -fs /phpfarm/inst/php-5.1.6/bin/php-cgi /phpfarm/inst/bin/php-cgi-5.1.6
+
