b7b7f82b2b958ada94d207341db62f57835fb73d
Eugene Sia <eugene@eugenesia.co.uk>
2017-03-26 17:30:40 +0100

    Fix mysqli ext statement in php.ini

--- phpfarm/custom/post-install-5.1.6.sh
+++ phpfarm/custom/post-install-5.1.6.sh
@@ -41,7 +41,7 @@ make
 cp modules/mysqli.so /phpfarm/inst/php-5.1.6/lib/
 
 # Load extension in php.ini.
-echo 'zend_extension = /phpfarm/inst/php-5.1.6/lib/mysqli.so' >> /phpfarm/inst/php-5.1.6/etc/php.ini
+echo 'extension = /phpfarm/inst/php-5.1.6/lib/mysqli.so' >> /phpfarm/inst/php-5.1.6/etc/php.ini
 
 
 #
