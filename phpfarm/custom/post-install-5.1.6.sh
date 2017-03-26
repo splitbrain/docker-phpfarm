824b0a04a2cd03265b9908b006f3c8868f4f6eff
Eugene Sia <eugene@eugenesia.co.uk>
2017-03-26 05:40:25 +0100

    Create links in shared bin dir for PHP 5.1

--- phpfarm/custom/post-install-5.1.6.sh
+++ phpfarm/custom/post-install-5.1.6.sh
@@ -3,5 +3,8 @@
 cp /phpfarm/src/php-5.1.6/sapi/cgi/php /phpfarm/inst/php-5.1.6/bin/php-cgi
 cp /phpfarm/src/php-5.1.6/sapi/cli/php /phpfarm/inst/php-5.1.6/bin/php
 
+# Create links in shared bin dir.
 ln -fs /phpfarm/inst/php-5.1.6/bin/php-cgi /phpfarm/inst/bin/php-cgi-5.1.6
+ln -fs /phpfarm/inst/php-5.1.6/bin/php-config /phpfarm/inst/bin/php-config-5.1.6
+ln -fs /phpfarm/inst/php-5.1.6/bin/phpize /phpfarm/inst/bin/phpize-5.1.6
 
