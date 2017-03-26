65ad36c3e2a9786ec8006dadbde16e98e6246c62
Eugene Sia <eugene@eugenesia.co.uk>
2017-03-26 02:20:19 +0100

    Tidy up PHP 5.1 options

--- phpfarm/custom/options-5.1.6.sh
+++ phpfarm/custom/options-5.1.6.sh
@@ -6,15 +6,22 @@
 patchDir='/phpfarm/src/custom/patches-5.1.6'
 
 patch -d $srcdir -p0 < $patchDir/curl.patch
-patch -d $srcdir -p0 < $patchDir/openssl.patch
+# patch -d $srcdir -p0 < $patchDir/openssl.patch
 patch -d $srcdir -p0 < $patchDir/pdo_oci.patch
 patch -d $srcdir -p0 < $patchDir/imap.patch
+# patch -d $srcdir -p0 < $patchDir/mysqli.patch
+# patch -d $srcdir -p0 < $patchDir/mysqli-2.patch
 
 
 # no intl on 5.1.6
 configoptions=`echo "$configoptions" |sed 's/--enable-intl//'`
 
-configoptions="$configoptions --enable-fastcgi"
+# OpenSSL and MySQLi options result in unresolvable errors due to
+# incompatibilities with existing libraries - leave them out.
+# MySQLi extension can be compiled later using PHPize, see http://gunner.me/archives/403
+configoptions=`echo "$configoptions" |sed 's/--with-openssl//'`
+configoptions=`echo "$configoptions" |sed 's/--with-mysqli//'`
+
 
 echo "--- loaded custom/options-5.1.6.sh ---"
 echo $configoptions
--- phpfarm/custom/patches-5.1.6/README.md
+++ phpfarm/custom/patches-5.1.6/README.md
@@ -1,8 +1,9 @@
-# PHP 5.1.6 patches
+# PHP 5.1.6 extension patches
 
-Patches for PHP 5.1.6 source for it to compile on Debian Wheezy.
+Patches for PHP 5.1.6 extension sources for it to compile on Debian Wheezy.
 
 References:
-* https://gist.github.com/0livier/4079937
-* http://yet.another.linux-nerd.com/blog/retro-php-compiling-5-dot-1-6-on-recent-linux/
+* Some ext patches: https://gist.github.com/0livier/4079937
+* Some ext patches (similar to above): http://yet.another.linux-nerd.com/blog/retro-php-compiling-5-dot-1-6-on-recent-linux/
+* Handling MySQLi ext: http://gunner.me/archives/403
 
