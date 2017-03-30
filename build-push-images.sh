878a060911ff20e31a43cc65de370a7594c8aa49
Eugene Sia <eugene@eugenesia.co.uk>
2017-03-30 03:25:34 +0000

    Allow setting Docker hub user/repo in build/push script

--- build-push-images.sh
+++ build-push-images.sh
@@ -2,15 +2,34 @@
 
 #
 # Build and push the images.
-# Turn on Docker experimental mode to enable the --squash functionality.
 #
+# Usage: build-push-images.sh mydockerhubuser/myreponame
+# E.g. build-push-images.sh eugenesia/phpfarm
+#
+# Turn on Docker experimental mode to enable the --squash functionality.
+# See https://github.com/docker/docker/tree/master/experimental
+#
+
+if [ -z "$1" ]; then
+
+  # Filename of this script.
+  myscript=`basename "$0"`
+
+  echo 'Please enter your Docker hub username and repo name in the format:'
+  echo "$myscript mydockerhubuser/myreponame"
+
+  exit 1
+fi
+
+# E.g eugenesia/phpfarm
+hubUserRepo="$1"
 
 set -vx
 
-docker build --squash -t eugenesia/phpfarm:jessie -f Dockerfile-Jessie . > /tmp/build-jessie.log 2>&1
-docker push eugenesia/phpfarm:jessie
+docker build --squash -t ${hubUserRepo}:jessie -f Dockerfile-Jessie . > /tmp/build-jessie.log 2>&1
+docker push ${hubUserRepo}:jessie
 
-docker build --squash -t eugenesia/phpfarm:wheezy -f Dockerfile-Wheezy . > /tmp/build-wheezy.log 2>&1
-docker push eugenesia/phpfarm:wheezy
+docker build --squash -t ${hubUserRepo}:wheezy -f Dockerfile-Wheezy . > /tmp/build-wheezy.log 2>&1
+docker push ${hubUserRepo}:wheezy
 
 set +vx
