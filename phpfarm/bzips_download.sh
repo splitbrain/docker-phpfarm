c5d1f62d06de97cf40e29dfcd7ed42dff36a862e
Eugene Sia <eugene@eugenesia.co.uk>
2017-07-25 14:56:13 +0100

    Add Bzip download script

---
+++ phpfarm/bzips_download.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Download Bzips for newer PHP versions not found in museum.php.net .
+# Those found in museum.php.net will be downloaded by compile.sh
+# See https://github.com/fpoirotte/phpfarm/blob/v0.2.0/src/compile.sh
+
+PHP_VERSIONS=(
+  5.6.31
+  7.0.21
+  7.1.7
+  7.2.0beta1
+)
+
+PHP_BZIPS=(
+  http://php.net/get/php-5.6.31.tar.bz2/from/this/mirror
+  http://php.net/get/php-7.0.21.tar.bz2/from/this/mirror
+  http://php.net/get/php-7.1.7.tar.bz2/from/this/mirror
+  https://downloads.php.net/~pollita/php-7.2.0beta1.tar.bz2
+)
+
+mkdir -p bzips
+cd bzips
+
+for KEY in ${!PHP_VERSIONS[@]}; do
+  VER=${PHP_VERSIONS[$KEY]}
+  BZIP_URL=${PHP_BZIPS[$KEY]}
+
+  wget -O php-$VER.tar.bz2 $BZIP_URL
+done
+
